89ae05b6d475b0a58903717d47dd61e070bf730e
Nick Kuntz <nicholas.kuntz@cloudflare.com>
2026-01-06 20:04:07 -0500

    Fix API token permission text to Magic WAN Read

--- src/index.ts
+++ src/index.ts
@@ -2290,7 +2290,7 @@ function getHtml(): string {
             <input type="password" class="form-input" id="apiToken" placeholder="API token with Magic WAN read access">
             <a href="https://developers.cloudflare.com/fundamentals/api/get-started/create-token/" target="_blank" class="docs-link">
               <svg class="docs-link-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><line x1="12" y1="17" x2="12.01" y2="17"/></svg>
-              Create token with "Magic Transit Read" permission
+              Create token with "Magic WAN Read" permission
             </a>
           </div>
         </div>
